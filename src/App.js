cef9d9bbd3c4bfcaef80189a9d55c03fd3807eb7
Sergey Zhuck <sergeyzhuck@gmail.com>
2022-05-29 01:52:34 +0300

    Clear project

--- src/App.js
+++ src/App.js
@@ -1,46 +1,15 @@
-import React, { useState, useMemo } from "react";
-import PostFilter from "./components/PostFilter";
-import PostForm from "./components/PostForm";
-import PostList from "./components/PostList";
+import React from "react";
+
 
 
 
 import "./styles/App.css"
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id:1, title:"bbbs", body:"555"},
-        {id:2, title:"ssss", body:"3333"},
-        {id:3, title:"ddddb", body:"4444"},
-    ]);
-    
-   const [filter, setFilter] = useState({sort:"", query:""})
-    
-
-    const sortedPosts = useMemo(()=>{
-        return filter.sort 
-            ? [...posts].sort((a,b) => {return a[filter.sort].localeCompare(b[filter.sort])})
-            : posts
-    }, [filter.sort, posts])
-
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-    }, [filter.query, sortedPosts])
-
-    const createPost = (newPost) => {
-        setPosts([...posts, newPost])
-    }
-
-    const removePost = (post) => {
-        setPosts(posts.filter(p => p.id !== post.id))
-    }
 
     return (
         <div className="App">
-            <PostForm create={createPost}/>
-            <hr style={{margin:"15px 0"}}/>
-            <PostFilter filter={filter} setFilter={setFilter}/>
-            <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
+            Hello world!
         </div>
     );
 }
